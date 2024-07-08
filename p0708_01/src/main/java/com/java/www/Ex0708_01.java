fc0e9bcce0b2f3901b4c8a3dc7d74ae848a08149
OldBeest <nijalmot@gmail.com>
2024-07-08 18:21:36 +0900

    20240708 spring

---
+++ p0708_01/src/main/java/com/java/www/Ex0708_01.java
@@ -0,0 +1,14 @@
+package com.java.www;
+
+public class Ex0708_01 {
+
+	public static void main(String[] args) {
+		
+//		Pencil6B pencil6b = new Pencil6B();
+//		pencil6b.use();
+		
+		Pencil pencil = new Pencil6B();
+		pencil.use();
+	}
+
+}
